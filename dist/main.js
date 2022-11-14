490b4214a1ceea7beb4837777e617df75d40766d
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-14 09:45:27 +0100

    Completed main parts of program

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this. priority = priority;
+    }
+} 
+/******/ })()
+;
